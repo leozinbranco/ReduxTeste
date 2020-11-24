d8222c5e7acaf32df277fa94e1e6ad07bcb7d8ba
Leonardo <55717308+leozinbranco@users.noreply.github.com>
2020-11-24 13:00:16 -0300

    finisih

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,15 @@
 import React from 'react';
 import Counter from './Counter';
 import "./App.css"
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+//import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware  } from 'redux';
+import rootReducer from './rootReducer';
 
-import { createStore } from 'redux';
-
-
-
-
+const initialState = {
+  count: 0
+};
 /*
 Função que irá retornar o state
 
@@ -21,13 +23,27 @@ ou seja, redux não "sabe nada" sobre react.
 
 a lib redux pode ser usada até mesmo fora do react app, trabalhando com vue, node, angular, etc
 */
-const initialState = {
-  count: 0
-};
 
-function reducer(state, action) {
+
+function reducer(state = initialState, action) {
   console.log('reducer', state, action);
-  return state;
+
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
 }
 /*
 Important Rule of Reducers #1: Never return undefined from a reducer.
@@ -38,17 +54,17 @@ Reducer: Reducer funciona da seguinte forma: É passado uma função que é cham
 
 
 Exemplo: 
-      var letters = ['r', 'e', 'd', 'u', 'x'];
+    var letters = ['r', 'e', 'd', 'u', 'x'];
 
-    #Recebe 2 argumentos:
-    -A função para fazer a redução(a reducer)
-    -Um valor inicial para o accumulatedResult
+  #Recebe 2 argumentos:
+  -A função para fazer a redução(a reducer)
+  -Um valor inicial para o accumulatedResult
 
-  var word = letters.reduce(
-        function(accumulatedResult, arrayItem) {
-          return accumulatedResult + arrayItem;
-        },
-  ''); // <-- string vazia = valor inicial. 
+var word = letters.reduce(
+      function(accumulatedResult, arrayItem) {
+        return accumulatedResult + arrayItem;
+      },
+''); // <-- string vazia = valor inicial. 
 
 console.log(word) 
 
@@ -67,50 +83,170 @@ Action é uma linguagem redux para um objeto simples com uma propriedade chamada
 
 Exemplo de ações:
 {
-  type: "add an item"
-  item: "Apple"
+type: "add an item"
+item: "Apple"
 }
 
 ou
 
 {
-  type: 7008
+type: 7008
 }
 
 ou 
 
 {
-  type: "INCREMENT"
+type: "INCREMENT"
 }
 
 Ações são muito livres, desde que tenham o tipo. 
 Para ser sustentavel, muitas vezes os tipos das açoes sao strings simples e muita vezes em letras maiusculas 
 
+  Handler:  Uma função com uma finalidade específica para tratar de algum problema.
+            Pode ser o clique de um botão, a alteração de propriedades de objetos, etc.
+
+
+
+
 AS ações nao fazem nada, e para fazer algo, precisa-se despachar ela
+-store.dispatch({ type: "INCREMENT" });
+Com isso, o redux irá chamar o reducer com tal ação, e substituirá o estado com o que o reducer retornou
+
+
+
+Handle Actions
+
+Para isso, há algumas maneiras de fazer isso. 
+Usar vários if/else para tratar o type da action;
+Criar um objeto no qual procura uma função pelo type do action; 
+Ou simplesmente um switch; 
+
+Exemplo:
+
+function reducer(state = initialState, action) {
+  console.log('reducer', state, action);
+
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+}
+
+
+Reducers tem que ser funções puras, isso significa que não pode modificar seus argumentos, e eles não podem ter um
+side effect.
+
+"side effect" = mudar variáveis fora do escopo, chamar outras funções que mudem coisas, funções que façam buscas (fetch),
+despachar ações, etc... (apenas console.log é permitido)
+
+NÃO pode ter side effect 
+
+NÃO MODIFIQUE O STATE ARGUMENT
+
+No mesmo conceito, você também não pode mudar o state por = state.count = 0, state.item.push(newItem), state.count++
+
+- Immer lib ajudaria a escrever reducers. 
+
+#Mais algumas regras...
+-Nunca mude o state, apenas retorne um novo.
+-tem a ideia de imutabilidade
+-Apenas ações podem "modificar" o state
+-Apenas uma maneira de mudanças: dispatch(action) -> reducer -> new state
+-Reducer deve ser uma pure functions 
 
 var letters = ['r', 'e', 'd', 'u', 'x'];
 
 var word = letters.reduce(
-    function(accumulatedResult, arrayItem) {
-        return accumulatedResult + arrayItem;
-      },
-      ''); // <-- string vazia = valor inicial.
-      
-      console.log(word) 
-      
-      */
+  function(accumulatedResult, arrayItem) {
+      return accumulatedResult + arrayItem;
+    },
+    ''); // <-- string vazia = valor inicial.
+    
+    console.log(word) 
+    
 
-const store = createStore(reducer);
+
+    #FETCH DATA with redux 
+
+    function getUser() {
+      return function() {
+        return fetch('/current_user');
+      };
+    }
+
+    Isso teoricamente não é suportado, porém é ai que entra o REDUX-THUNK. 
+    É um middleware, basicamente um plugin para redux, que ativa redux para 
+    usar ações como: getUser()
+
+    Você pode despachar essas thunk actions
+    - dispatch(getUser())
+
+
+    Mas, o que é Thunk?
+    Thunk é um nome comum para uma função que retorna outra função
+
+    function doStuff() { // action creator
+      return function(dispatch, getState) { //thunk 
+        //Na maioria das vezes, apenas é usado o dispatch de argumento, porém, 
+        quando se quer algo com condição baseado em algum valor do redux state, 
+        apenas use getState, e assim você terá acesso ao estado inteiro para ler. 
+
+
+        // dispatch actions here
+        // or fetch data
+        // or whatever
+      }
+    }
+
+      #Dispatch the Action to Fetch the Data
+      - fetchProducts deve ser usado dntro do componentDidMount method, e se estiver usando hooks, useEffect().
+      - Em casos de dados globais, em que todo o aplicativo precisa, despache a ação logo depois de criar o store, 
+        com store.dispatch
+
+      #Como nomear Redux Actions
+      - Redux fetch data é normalmente usado com BEGIN, SUCCESS, FAILURE.
+      - BEGIN/SUCCESS/FAILURE é bom, pois ao usar uma loading flag, onde a resposta é true é setado BEGIN action, e 
+        false depois de SUCCESS ou FAILURE 
+      - ANTES de começar a requisição API, despache BEGIN 
+      - DEPOIS da requisição for bem sucedida, despache SUCCESS com os dados, e se falhou, despache FAILURE com erro. 
+      - 
+
+
+    */
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk) 
+  //thunk tem que estar wrap no applyMiddleware, não passe thunk diretamente. 
+  );
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+store.dispatch({ type: "RESET" });
+store.dispatch(rootReducer);
+
 
 function App() {
-
+  /*
+    Para todos terem acesso ao state, é necessário usar o connect 
+  */
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={Counter} />
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   );
 }
 
--- my-app/src/Counter/index.js
+++ my-app/src/Counter/index.js
@@ -1,34 +1,68 @@
 import React from 'react';
 import './Counter.css';
+import { connect } from 'react-redux';
+import { increment, decrement, reset } from '../actions';
 
 class Counter extends React.Component {
-  const = { count: 0 }
-  
-  
+  //const = { count: 0 } remove this
+
+  /*Preparando o componente para o Redux */
+
+
 
   increment = () => {
+    /*
+    Remove this 
     this.setState({
       count: this.state.count + 1
     });
+
+    Don’t dispatch(increment) 🚫
+
+    Do dispatch(increment()) ✅
+    */
+    //this.props.dispatch(increment());
+    this.props.increment();
   }
   /*
-    Quando um estado muda, o react renderiza novamente com os novos estados. 
+  Quando um estado muda, o react renderiza novamente com os novos estados. 
   */
-  decrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
+  decrement = () => { /*
+  Also remove this 
+  this.setState({
+    count: this.state.count - 1
+  });*/
+    //this.props.dispatch(decrement());
+    this.props.decrement();
+
   }
 
+  reset = () => {
+    //this.props.dispatch(reset());
+    this.props.reset();
+  }
+
+
+
   render() {
+
+
+
     return (
       <div className="counter-container">
         <div className="items-container">
-          <h2>Counter</h2> 
-          <div style={{flexDirection:'column'}}>
-          <button onClick={this.decrement}>-</button>
-          <span style={{margin:10}}>{this.state.count}</span>
-          <button onClick={this.increment}>+</button>
+          <h2>Counter</h2>
+          <div style={{ flexDirection: 'column' }}>
+            <button onClick={this.decrement}>-</button>
+            <span style={{ margin: 10 }}>{/*
+          this.state.count
+          */
+              this.props.count
+            }</span>
+            <button onClick={this.increment}>+</button>
+            <div>
+              <button onClick={this.reset}>RESET</button>
+            </div>
 
           </div>
         </div>
@@ -37,4 +71,31 @@ class Counter extends React.Component {
   }
 }
 
-export default Counter;
+// Add this function:
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  };
+}
+
+
+
+//Não é necessário, mas é mais um level abstrato 
+const mapDispatchToProps = { //Pode ser uma function também
+  increment,
+  decrement,
+  reset,
+};
+
+//Isso é legal pois poupa você de chamar o dispatch() manualmente 
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+//retorna uma função quando você a chama = high order function
+//o connect é um hook para o redux, ou seja, retirar o state e passar por meio dessa função.(mapStateToProps)
+//O objeto retornado da função mapStateToProps é preenchido no meu componente como props. Pega o state object e retorna um objeto cheio de 
+//adereços
+
+//Não precisa de dispatch, tendo em vista que o mapStateToProps já copia o estado e passa um objeto identico 
+//
+
+  //export default Counter;
